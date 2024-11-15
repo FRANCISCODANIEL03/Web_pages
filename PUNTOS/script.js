55cc85a392e6c1aab585a06ed237ee25c8e76ac2
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-11-14 18:54:50 -0600

    create multiple balls

--- PUNTOS/script.js
+++ PUNTOS/script.js
@@ -4,5 +4,17 @@ const ctx = canvas.getContext("2d")
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-let bola = new Bola(canvas.width / 2, canvas.height / 2)
-bola.dibujar()
+let balls = []
+
+for(let i = 0; i < 5; i++){
+    balls.push(new Bola(canvas.width / 2, canvas.height / 2))
+}
+function animar(){
+    balls.forEach(ball =>{
+        ball.dibujar()
+        ball.move()
+    })
+    requestAnimationFrame(animar)
+}
+
+animar()
