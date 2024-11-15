5db76053c90ed9f22a19a9127992cf4ba2317463
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-11-14 19:01:06 -0600

    add more balls

--- PUNTOS/script.js
+++ PUNTOS/script.js
@@ -6,10 +6,12 @@ canvas.height = innerHeight
 
 let balls = []
 
-for(let i = 0; i < 5; i++){
+for(let i = 0; i < 50; i++){
     balls.push(new Bola(canvas.width / 2, canvas.height / 2))
 }
 function animar(){
+
+    ctx.clearRect(0,0, canvas.width, canvas.height)
     balls.forEach(ball =>{
         ball.dibujar()
         ball.move()
