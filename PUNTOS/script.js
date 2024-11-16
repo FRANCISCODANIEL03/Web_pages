f2ff6963093f9f90bdb540247d564c9976d70bf0
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-11-15 19:05:34 -0600

    add stroke between balls

--- PUNTOS/script.js
+++ PUNTOS/script.js
@@ -6,13 +6,20 @@ canvas.height = innerHeight
 
 let balls = []
 
-for(let i = 0; i < 50; i++){
+for(let i = 0; i < 15; i++){
     balls.push(new Bola(canvas.width / 2, canvas.height / 2))
 }
 function animar(){
 
     ctx.clearRect(0,0, canvas.width, canvas.height)
     balls.forEach(ball =>{
+        balls.forEach(bl =>{
+            ctx.beginPath()
+            ctx.moveTo(ball.x,ball.y)
+            ctx.lineTo(bl.x,bl.y)
+            ctx.stroke()
+            ctx.closePath()
+        })
         ball.dibujar()
         ball.move()
     })
