ce9ec0bdf93c9d3406879d534030bbb100de626a
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-11-15 19:11:54 -0600

    change stroke between balls

--- PUNTOS/script.js
+++ PUNTOS/script.js
@@ -6,7 +6,7 @@ canvas.height = innerHeight
 
 let balls = []
 
-for(let i = 0; i < 15; i++){
+for(let i = 0; i < 20; i++){
     balls.push(new Bola(canvas.width / 2, canvas.height / 2))
 }
 function animar(){
@@ -14,11 +14,17 @@ function animar(){
     ctx.clearRect(0,0, canvas.width, canvas.height)
     balls.forEach(ball =>{
         balls.forEach(bl =>{
-            ctx.beginPath()
-            ctx.moveTo(ball.x,ball.y)
-            ctx.lineTo(bl.x,bl.y)
-            ctx.stroke()
-            ctx.closePath()
+            let dx = bl.x - ball.x
+            let dy = bl.y - ball.y
+            let dist = Math.sqrt(dx ** 2 + dy ** 2)
+
+            if(dist < 300){
+                ctx.beginPath()
+                ctx.moveTo(ball.x,ball.y)
+                ctx.lineTo(bl.x,bl.y)
+                ctx.stroke()
+                ctx.closePath()
+            }
         })
         ball.dibujar()
         ball.move()
