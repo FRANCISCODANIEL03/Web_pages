73149101d5956d0c2738bbce3121a0d46d8071e8
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-11-21 00:59:48 -0600

    define variables

---
+++ CALCULATOR/script.js
@@ -0,0 +1,11 @@
+window.onload=()=>{
+    const themeToggle = document.getElementById('theme-toggle')
+    const body = document.body
+    const expressionDisplay = document.getElementById('expression')
+    const resultDisplay = document.getElementById('result')
+    const buttons = document.querySelectorAll('.buttons button')
+    let expression = ""
+    let result = ""
+    let lastInput = ""
+
+}
