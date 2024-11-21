775ee8eeaaa81a8f9767a1e73f38640c356b0df7
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-11-21 01:02:16 -0600

    add functionality to each button

--- CALCULATOR/script.js
+++ CALCULATOR/script.js
@@ -14,4 +14,41 @@ window.onload=()=>{
         themeText.textContent = themeToggle.checked ? "Switch to Ligth" : "Switch to Dark"
     })
 
+    buttons.forEach(button =>{
+        button.addEventListener('click', ()=>{
+            const value = button.textContent
+            if(value === 'AC'){
+                expression = ""
+                result = ""
+                lastInput = ""
+            }else if (value === '='){
+                try{
+                    const formattedExpression = expression.replace(/(\d+)%/g, (match, num) => num /100)
+                    result = eval(formattedExpression).toString()
+                }catch(error){
+                    result = 'Syntax Error'
+                }
+            }else if(!['+','-','*','/'].includes(value)){
+                if(!['+','-','*','/'].includes(lastInput)){
+                    expression += value
+                }
+            }else if(value === '%'){
+                if(/\d$/.test(expression)){
+                    expression += '%'
+                }
+            }else {
+                expression += value
+            }
+            lastInput = value
+            expressionDisplay.textContent = expression
+            resultDisplay.textContent = result
+            if(expression.length > 10 || result.length > 10){
+                expressionDisplay.classList.add('long')
+                resultDisplay.classList.add('long')
+            }else{
+                expressionDisplay.classList.remove('long')
+                resultDisplay.classList.remove('long')
+            }
+        })
+    })
 }
