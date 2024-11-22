20260c4bad5f1873dc8edd32fc10ee70f2a2ccf1
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-11-22 02:05:49 -0600

    define constants

--- CLOCK/script.js
+++ CLOCK/script.js
@@ -1,3 +1,8 @@
 const $ = (selector) =>{
     return document.querySelector(selector)
 }
+const hour = $('.hour')
+const dot = $('.dot')
+const min = $('.min')
+const week = $('.week')
+let showDot = true
