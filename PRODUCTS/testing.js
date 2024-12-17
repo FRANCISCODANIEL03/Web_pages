6662b6c1945e18fc067f57d501c43f77eb8a9912
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2024-12-17 02:23:48 -0600

    add constants

---
+++ PRODUCTS/testing.js
@@ -0,0 +1,13 @@
+let title = document.getElementById('title')
+let total = document.getElementById('total')
+let count = document.getElementById('count')
+let category = document.getElementById('category')
+let button = document.getElementById('submit')
+
+let price = document.getElementById('price')
+let taxes = document.getElementById('taxes')
+let ads = document.getElementById('ads')
+let discount = document.getElementById('discount')
+
+let mode = 'Create'
+let globalI
