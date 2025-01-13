d8d6487d1cb8ebf89e2c8c29811d8314d3dc02a2
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-01-12 19:54:03 -0600

    define variables and constants

---
+++ PAGINA_TIENDA/admin.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    const cartButton = document.getElementById("cart-button");
+    const cart = document.getElementById("cart");
+    const logoutButton = document.getElementById("logout-button");
+    const Result = document.getElementById("result_list");
+    const divRes = document.getElementById("result");
+    const prodButton = document.getElementById("prod");
+    const storeButton = document.getElementById("store");
+    const buyButton = document.getElementById("buy");
+    const clientButton = document.getElementById("cliente");
+    const searchButton = document.getElementById("search");
+    const insButton = document.getElementById("insert");
+    const upButton = document.getElementById("update");
+    const delButton = document.getElementById("delete");
+    const opButton = document.getElementById("btn_op");
+    const nameL = document.getElementById("name");
+    const divId = document.getElementById("did");
+    const divNom = document.getElementById("dnombre");
+    const divApe1 = document.getElementById("dape1");
+    const divApe2 = document.getElementById("dape2");
+    const divFn = document.getElementById("dfecha");
+    const divNomT = document.getElementById("dnombret");
+    const divPunt = document.getElementById("dpuntos");
+    const divNomP = document.getElementById("dnombrep");
+    const divDes = document.getElementById("ddescrip");
+    const divPrecio = document.getElementById("dprecio");
+    const divStock = document.getElementById("dstock");
+    const oper = document.getElementById("opera");
+
+    let cont = 0;
+    let rec = "";
+    let op = "";
+    let producto = [];
+    let tienda = [];
+    let compra = [];
+    let cliente = [];
+    let data = []
+
+
+});
