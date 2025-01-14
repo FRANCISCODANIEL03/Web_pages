2540cecf059fcff4c6dfcc4b4ad24101070d3062
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-01-13 20:12:03 -0600

    add funcionality to the button "actualizar"

--- PAGINA_TIENDA/admin.js
+++ PAGINA_TIENDA/admin.js
@@ -129,5 +129,35 @@ document.addEventListener("DOMContentLoaded", async () => {
          opButton.style.display = "block"
     })
 
+    upButton.addEventListener("click", () =>{
+        hiddenI()
+        oper.textContent = "ACTUALIZAR"
+        oper.style.display = "block"
+        op = "update"
+        divId.style.display = "block"
+        switch(cont){
+            case 1:
+                divNomP.style.display = "block"
+                divDes.style.display = "block"
+                divPrecio.style.display = "block"
+                divStock.style.display = "block"
+                rec = "product"
+            break;
+            case 2:
+                divNomT.style.display = "block"
+                rec = "store"
+            break;
+            case 4:
+                divNom.style.display = "block"
+                divApe1.style.display = "block"
+                divApe2.style.display = "block"
+                divFn.style.display = "block"
+                divPunt.style.display = "block"
+                rec = "client"
+            break;
+        }
+        opButton.style.display = "block"
+    })
+
 
 });
