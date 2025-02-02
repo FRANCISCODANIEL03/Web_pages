b554c9b454c4ed00caea9cb0738b36593f67408a
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-02-01 21:51:27 -0600

    define constants

---
+++ PAGINA_TIENDA/register.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    const regButton = document.getElementById("reg");
+    const nombre = document.getElementById("nombre");
+    const apellido1 = document.getElementById("ape1");
+    var apellido2 = document.getElementById("ape2").value;
+    
+});
