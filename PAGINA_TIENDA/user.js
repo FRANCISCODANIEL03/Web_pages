2a4460e1e77feae6866fbc10f45d12a780472499
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-02-02 21:53:12 -0600

    define constants

---
+++ PAGINA_TIENDA/user.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    const userIdInput = document.getElementById("user-input");
+    const userNameInput = document.getElementById("user-name");
+    const loginButton = document.getElementById("login");
+
+    let clientes = [];
+
+});
