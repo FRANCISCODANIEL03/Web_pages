641e0ab3627dcca71a460ef35b77101ab0ae8425
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-02-25 18:24:40 -0600

    add event

--- SEARCH_FILTER/script.js
+++ SEARCH_FILTER/script.js
@@ -1,3 +1,15 @@
 const searchInput = document.getElementById('searchInput');
 const itemList = document.getElementById('itemList');
 const items = document.getElementsByTagName('li');
+
+searchInput.addEventListener('input', ()=>{
+    const filter = searchInput.value.toLowerCase();
+    Array.from(items).forEach((item)=>{
+        const text = item.textContent.toLowerCase();
+        if (text.includes(filter)){
+            item.classList.remove('hidden');
+        } else{
+            item.classList.add('hidden');
+        }
+    })
+})
