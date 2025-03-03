727146b06573474b1f7299c9ef5f632410082d18
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-03-02 18:53:45 -0600

    define constant

--- MENU/index.js
+++ MENU/index.js
@@ -1,3 +1,4 @@
 const sidebar = document.getElementById('sidebar');
 const btnClose = document.getElementById('btn-close');
 const btnOpen = document.getElementById('btn-open');
+const btnLigthMode = document.getElementById('ligth-mode');
