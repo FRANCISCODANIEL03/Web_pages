121dd7771d807e566b0423ffeae5f5d73be2242f
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-03-14 19:21:18 -0600

    define variable

--- BARRA_LATERAL/script.js
+++ BARRA_LATERAL/script.js
@@ -2,4 +2,5 @@ const sidebar = document.querySelector('.sidebar');
 const sidebarToggler = document.querySelector('.sidebar-toggler');
 const menuToggler = document.querySelector('.menu-toggler');
 
+let collapsedSidebarHeigth = '56px';
 
