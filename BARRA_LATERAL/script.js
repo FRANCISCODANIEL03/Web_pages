155bc038e96832ea6bdc30f30347ddb8688ffa7e
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-03-14 19:20:59 -0600

    define constant

--- BARRA_LATERAL/script.js
+++ BARRA_LATERAL/script.js
@@ -1,2 +1,5 @@
 const sidebar = document.querySelector('.sidebar');
 const sidebarToggler = document.querySelector('.sidebar-toggler');
+const menuToggler = document.querySelector('.menu-toggler');
+
+
