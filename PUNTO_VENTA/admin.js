135806c3750f343593a047f861fed34bddee18e4
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-09-21 09:41:05 -0600

    add new constant

--- PUNTO_VENTA/admin.js
+++ PUNTO_VENTA/admin.js
@@ -2,4 +2,5 @@ document.addEventListener("DOMContentLoaded", async () => {
     const cartButton = document.getElementById("cart-button");
     const cart = document.getElementById("cart");
     const logoutButton = document.getElementById("logout-button");
+    const Result = document.getElementById("result_list");
 });
