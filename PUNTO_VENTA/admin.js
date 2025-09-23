e6ba03b1d17c8d1b2018d0f623021550d1be926a
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-09-23 09:48:02 -0600

    add new constant

--- PUNTO_VENTA/admin.js
+++ PUNTO_VENTA/admin.js
@@ -20,5 +20,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     const divApe1 = document.getElementById("dape1");
     const divApe2 = document.getElementById("dape2");
     const divEm = document.getElementById("demail");
+    const divPass = document.getElementById("dpass");
+
 
 });
