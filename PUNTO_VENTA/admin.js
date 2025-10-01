c8bee45514b706f9aa2a8f42dc490793c24e08f8
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-10-01 10:20:14 -0600

    add funcionality

--- PUNTO_VENTA/admin.js
+++ PUNTO_VENTA/admin.js
@@ -278,6 +278,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         hiddenI();
         oper.textContent = "ELIMINAR";
         oper.style.display = "block";
+        op = "delete";
     });
 
 
