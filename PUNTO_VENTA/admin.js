10274e26c7393f47cffa528d2c79bb629e572fec
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-10-03 10:26:26 -0600

    add funcionality

--- PUNTO_VENTA/admin.js
+++ PUNTO_VENTA/admin.js
@@ -316,5 +316,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     opButton.addEventListener("click", async () => {
         const Id = document.getElementById("id").value;
         const Nom = document.getElementById("nombre").value;
+        const Ape1 = document.getElementById("ape1").value;
+
 
 });
