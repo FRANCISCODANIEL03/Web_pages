da16931ca30a8e838c61db1730609ea3223b271b
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-10-03 10:27:02 -0600

    add funcionality

--- PUNTO_VENTA/admin.js
+++ PUNTO_VENTA/admin.js
@@ -318,5 +318,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         const Nom = document.getElementById("nombre").value;
         const Ape1 = document.getElementById("ape1").value;
         var Ape2 = document.getElementById("ape2").value;
+        const NomP = document.getElementById("nombrep").value;
+
 
 });
