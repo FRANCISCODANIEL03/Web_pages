016005d6d6731a64ee90773fb971f8015291eb0e
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-10-10 10:46:51 -0600

    add funcionality

--- PUNTO_VENTA/admin.js
+++ PUNTO_VENTA/admin.js
@@ -545,6 +545,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                     body: JSON.stringify(user)
                 });
                 if (!response.ok) throw new Error("Error al obtener los datos de los clientes");
+                clientes = await response.json();
             } 
         }
        
