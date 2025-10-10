aee2b9ed89b6fdc9366fc7030de5baad081c2629
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-10-10 10:46:07 -0600

    add funcionality

--- PUNTO_VENTA/admin.js
+++ PUNTO_VENTA/admin.js
@@ -531,6 +531,10 @@ document.addEventListener("DOMContentLoaded", async () => {
                 return;
             }
 
+            const user = {
+                "email": email,
+                "password": password
+            }
         }
        
 
