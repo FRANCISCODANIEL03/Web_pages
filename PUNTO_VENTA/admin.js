ce767fc91b63b63d542350f9bc82fd70fbb0c01c
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-10-13 11:00:05 -0600

    add funcionality

--- PUNTO_VENTA/admin.js
+++ PUNTO_VENTA/admin.js
@@ -672,6 +672,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     function mostrarConfirmacion(mensaje) {
         return new Promise((resolve) => {
+            const modal = document.getElementById("confirmModal");
         });
     }
 
