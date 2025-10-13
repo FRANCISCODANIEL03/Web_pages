0450831cc14068523024de8fe1f9bf6e8dcaad79
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-10-13 10:57:27 -0600

    add funcionality

--- PUNTO_VENTA/admin.js
+++ PUNTO_VENTA/admin.js
@@ -632,6 +632,13 @@ document.addEventListener("DOMContentLoaded", async () => {
                 );
             }
         } else if (op == "delete" && rec == "buy") {
+            if (ID < 1 || isNaN(ID)) {
+                notyf.open({
+                    type: "warning",
+                    message: "Por favor, ingresa un id valido",
+                });
+                return;
+            }
         }
     });
 
