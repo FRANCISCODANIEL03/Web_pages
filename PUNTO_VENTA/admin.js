c6f45ef193c117b326da4496cfc79cf1e3a79da8
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-10-14 11:02:27 -0600

    add logout functionality to redirect to login page

--- PUNTO_VENTA/admin.js
+++ PUNTO_VENTA/admin.js
@@ -709,4 +709,11 @@ document.addEventListener("DOMContentLoaded", async () => {
         }, 4000);
     });
 
+    // Cerrar sesión
+    logoutButton.addEventListener("click", () => {
+        // Redirigir a la página de inicio de sesión
+        window.location.href = "index.html";
+    });
+
+
 });
