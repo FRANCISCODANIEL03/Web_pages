0ecf650f7ba81ad63f255fa24abee760a25cd068
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-10-26 21:57:34 -0600

    add code

--- PUNTO_VENTA/login.js
+++ PUNTO_VENTA/login.js
@@ -2,4 +2,5 @@ document.addEventListener("DOMContentLoaded", async () => {
     const URL = "http://localhost:3001"
     const cartButton = document.getElementById("cart-button");
     const cart = document.getElementById("cart");
+    const logoutButton = document.getElementById("logout-button");
 });
