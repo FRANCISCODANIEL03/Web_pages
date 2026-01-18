cdc2a9cd8654a78c21abee48a23d0a04bce4b06d
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2026-01-17 18:49:18 -0600

    add code

--- PUNTO_VENTA/login.js
+++ PUNTO_VENTA/login.js
@@ -223,6 +223,7 @@ document.addEventListener("DOMContentLoaded", async () => {
             notyf.success(`Agregado al carrito: ${productName} - Cantidad: ${quantity}`);
 
             const li = document.querySelector(`li[data-id="${productId}"]`);
+            if (li) aplicarEstiloStock(li, newStock);
 
         }
 
