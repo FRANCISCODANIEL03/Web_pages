42356be0580fe674d4058976fc4311e4ba8eb543
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2026-01-23 19:46:25 -0600

    add code

--- PUNTO_VENTA/login.js
+++ PUNTO_VENTA/login.js
@@ -491,6 +491,7 @@ document.addEventListener("DOMContentLoaded", async () => {
             console.log(data2);
 
             if (!response.ok) throw new Error("Error al realizar la compra");
+            notyf.success(`Compra realizada con éxito con el ID: ${data2.id_compra}`);
         } catch (error) {
             console.error("Error:", error)
             notyf.error('Hubo un problema al realizar la compra.');
