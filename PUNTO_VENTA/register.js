dd34529a00246246315df0c08729e0ba6a6fe821
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2026-01-25 19:53:59 -0600

    add code

--- PUNTO_VENTA/register.js
+++ PUNTO_VENTA/register.js
@@ -2,4 +2,5 @@ document.addEventListener("DOMContentLoaded", async () => {
     const URL = "http://localhost:3001"
     const passwordInput = document.getElementById('password');
     const confirmPasswordInput = document.getElementById('confirm-pass');
+    const toggleButton = document.getElementById('togglePassword');
 });
