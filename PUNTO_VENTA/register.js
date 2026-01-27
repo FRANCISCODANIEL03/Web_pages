2a1a86b9d5d92441c57bff10546a7e274f7769cc
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2026-01-26 19:57:41 -0600

    add code

--- PUNTO_VENTA/register.js
+++ PUNTO_VENTA/register.js
@@ -24,6 +24,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     toggleButton2.addEventListener('click', () => {
         const isPassword = confirmPasswordInput.type === 'password';
+        confirmPasswordInput.type = !isPassword ? 'password' : 'text';
     });
 
 
