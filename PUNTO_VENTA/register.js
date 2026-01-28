5802987aac140630455a3fdb7620477c98c2085a
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2026-01-27 19:59:29 -0600

    add code

--- PUNTO_VENTA/register.js
+++ PUNTO_VENTA/register.js
@@ -45,5 +45,6 @@ document.addEventListener("DOMContentLoaded", async () => {
         });
         const nombre = document.getElementById("nombre").value;
         const apellido1 = document.getElementById("ape1").value;
+        const email = document.getElementById("email").value;
     });
 });
