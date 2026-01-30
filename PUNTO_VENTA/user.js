cb613d4039d44b7537d5d3690a6b4a2de88d97cb
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2026-01-29 20:56:24 -0600

    add code

--- PUNTO_VENTA/user.js
+++ PUNTO_VENTA/user.js
@@ -2,5 +2,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     const URL = "http://localhost:3001"
     const passwordInput = document.getElementById('password');
     const toggleButton = document.getElementById('togglePassword');
+    const icon = document.getElementById('iconPassword');
 
 });
