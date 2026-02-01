aeae7ff8d65464c10b367590699e12dc871bb1e3
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2026-01-31 21:01:51 -0600

    add code

--- PUNTO_VENTA/user.js
+++ PUNTO_VENTA/user.js
@@ -71,6 +71,7 @@ document.addEventListener("DOMContentLoaded", async () => {
             notyf.success('Inicio de administrador exitoso.')
             // Almacenar los datos del usuario en localStorage
             localStorage.setItem("clienteId", clientes.id);
+            localStorage.setItem("clienteNombre", clientes.nombre);
         }
 
     });
